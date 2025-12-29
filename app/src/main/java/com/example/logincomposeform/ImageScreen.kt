87802f831c24e42d3f6aa3a1168fda31909b0b5b
praneeth123P <gorasapraneethswami@gmail.com>
2025-12-29 17:23:54 +0530

    Code Changes

--- app/src/main/java/com/example/logincomposeform/ImageScreen.kt
+++ app/src/main/java/com/example/logincomposeform/ImageScreen.kt
@@ -6,11 +6,15 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
@@ -40,5 +44,16 @@ class ImageScreen:ComponentActivity()
 @Composable
 fun ImageView()
 {
+   Column(
+       modifier = Modifier.padding(50.dp),
+       horizontalAlignment = Alignment.CenterHorizontally,
+       verticalArrangement =  Arrangement.Center,
+   )
+   {
+       Text(
+           text = " Android Developer image",
+           color = Color.Cyan,
+       )
+   }
 
 }
