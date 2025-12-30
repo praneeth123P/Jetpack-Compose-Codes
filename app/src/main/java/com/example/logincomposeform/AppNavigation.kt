1e4456226d155d305de4a945c325297dea39b3d6
praneeth123P <gorasapraneethswami@gmail.com>
2025-12-30 17:01:00 +0530

    Intial Commit

---
+++ app/src/main/java/com/example/logincomposeform/AppNavigation.kt
@@ -0,0 +1,30 @@
+package com.example.logincomposeform
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun AppNavigation()
+{
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "image"
+    ) {
+
+        composable("image") {
+            ImageView(
+                onNextClick = {
+                    navController.navigate("alert")
+                }
+            )
+        }
+
+        composable("alert") {
+            Alertdialog()
+        }
+    }
+}
