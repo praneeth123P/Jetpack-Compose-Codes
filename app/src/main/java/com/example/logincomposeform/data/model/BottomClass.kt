5973b864c14728b9a1c4d8722017fee364009e5b
praneeth123P <gorasapraneethswami@gmail.com>
2026-01-01 13:19:32 +0530

    Intial Commit Data Class of BottomClass File

---
+++ app/src/main/java/com/example/logincomposeform/data/model/BottomClass.kt
@@ -0,0 +1,9 @@
+package com.example.logincomposeform.data.model
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class BottomClass(
+    val label : String,
+    val icon : ImageVector,
+    val route : String
+)
